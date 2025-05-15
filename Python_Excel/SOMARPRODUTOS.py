96d4d6bde7fef00df4f714fb91f37ce98b624225
Augusto-Tinoco <augustolivera11@gmail.com>
2025-05-15 13:55:51 -0300

    Dei uma explica nas condicionais

--- Python_Excel/SOMARPRODUTOS.py
+++ Python_Excel/SOMARPRODUTOS.py
@@ -3,9 +3,10 @@ acumulador_par = acumulador_impar =  0
 
 
 for num in lista:
-    if num % 2 == 0:
+    if num % 2 == 0: #Se a divisão do número por 2 der resto 0, o número é par
         acumulador_par += num
-    else:
+   
+    else: #Do contrário, é impar
         acumulador_impar += num
 print('Os números são : ',lista)
 print('Soma dos números pares :', acumulador_par)
