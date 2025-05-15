89f326f413a2dd1191a1b09c2a2fbea25fb33854
Augusto-Tinoco <augustolivera11@gmail.com>
2025-05-15 02:48:56 -0300

    Atualizando o código - Explicando If e Else

--- Python_Excel/SOMARPRODUTOS.py
+++ Python_Excel/SOMARPRODUTOS.py
@@ -1,11 +1,15 @@
 lista = [3,8,5,10,7,4]
+
 acumulador_par = acumulador_impar = 0
 
 for x in range (0, len(lista)):
 
-    if(lista[x]%2==0):
+    if(lista[x]%2==0): #Caso um item x (0,1,2,3...) da lista seja par, ele irá ser somado ao acumulador_par
         acumulador_par += lista[x]
+
     else:
-        acumulador_impar += lista[x]
+        acumulador_impar += lista[x] #Caso contrário, ele irá ser somado ao acumulador_impar
+
 print('Soma dos números pares : ',acumulador_par)
+
 print('Soma dos números ímpares : ',acumulador_impar)
